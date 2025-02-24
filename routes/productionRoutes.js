47a4f1597f9ce0bb04e63c480f9506934aec23d4
Sanuda <sanudatharin@gmail.com>
2025-02-24 09:45:39 +0530

    add route for cutter machine

--- routes/productionRoutes.js
+++ routes/productionRoutes.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const { insertPunchingData, getPunchingData } = require("../controllers/PunchingController");
 const { insertCnDData, getCnDData } = require("../controllers/CnDController");
+const { insertMach3Data, getMach3Data } = require("../controllers/CutterController");
 
-// POST route to insert data
+// GET and POST route to insert Punching data
 router.post("/data", insertPunchingData);
-
-// GET route to retrieve data
 router.get("/punching", getPunchingData);
 
 // GET and POST for Cutting and Drilling Machine
 router.post("/postCnD", insertCnDData);
 router.get("/getCnD", getCnDData);
+
+//GET and POST for machine 3
+router.post("/postCutter", insertCutterData);
+router.get("/getCutter", getCutterData);
+
 
 module.exports = router;
