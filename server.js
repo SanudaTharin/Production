8185617bf62e7d571fbf83f79a8a8d54515e261f
Sanuda <sanudatharin@gmail.com>
2025-03-10 16:07:41 +0530

    bug fix

--- server.js
+++ server.js
@@ -8,14 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-//Use routes
+// Middleware to set Content-Security-Policy header
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "upgrade-insecure-requests;");
+  next();
+});
+
+// Use routes
 app.use("/production", productionRoutes);
 
 const PORT = 3000;
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
 
 
 
